d6d7b6699a18b8cff8c645efb6dcc77811bd0803
jjswan <69625230+jjswan@users.noreply.github.com>
2024-11-07 18:11:45 +0900

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def main():
     st.set_page_config(page_title="장지윤 test", page_icon="🥨")  # 웹 페이지 제목과 아이콘 설정
     st.image('energy.png')  # 상단에 이미지를 표시
     st.title("_:blue[에너지 학습 도우미]_ 🏫")  # 제목 표시 (에너지 학습 도우미)
-    st.header("😶주의! 이 챗봇은 참고용으로 사용하세요!", divider='rainbow')  # 주의사항 표시
+    st.header("챗봇을 사용해보세요!", divider='rainbow')  # 주의사항 표시
 
     # 세션 상태 초기화
     # Streamlit 세션에서 대화 상태, 대화 기록, 처리 완료 여부 등을 초기화하여 유지
